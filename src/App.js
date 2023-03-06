5701a3d45dd8189c7705475725626b1601f487ef
Prasad De Silva <68392136+accpdesilva@users.noreply.github.com>
2023-03-07 01:23:24 +0530

    XML converter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import PDFFile from './components/PDFFile';
+import XMLParser from './components/XMLParser';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 
 const App = ()  => {
---
+++ src/components/XMLParser.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import XMLParser from 'react-xml-parser';
+
+function Api() {
+
+    useEffect(() => {
+        fetch("https://russellinvestments.com/webapi/ChiliPublishFeed/GetChiliData?itemId=746B61D4-FFE8-4E90-9C71-67A68FE04A19")
+            .then(res => res.text())
+            .then(data => {
+                var xml = new XMLParser().parseFromString(data); 
+                console.log(xml)
+            })
+            .catch(err => console.log(err));
+    }, [])
+
+    return (
+        <div></div>
+    )
+}
+
+export default XMLParser
